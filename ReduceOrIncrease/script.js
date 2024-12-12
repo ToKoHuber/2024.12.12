8b1c32a31d4a382ae34fc28cf8ea7e20d4a9335b
24LP2879 24LP2879 <24LP2879@G304-CC0235.local>
2024-12-12 12:43:37 +0800

    office update

--- ReduceOrIncrease/script.js
+++ ReduceOrIncrease/script.js
@@ -1,15 +1,23 @@
-const main = document.getElementById("container");
+const container = document.getElementById("container");
 const increase = document.getElementById("increase");
 const decrease = document.getElementById("decrease");
 const too = document.getElementById("too");
+
 let a = 12;
 too.innerText = a;
 
 increase.addEventListener("click", () => {
   a = a + 1;
+
   too.innerText = a;
+  const box = document.createElement("div");
+  box.setAttribute("class", "card");
+  container.appendChild(box);
 });
 decrease.addEventListener("click", () => {
   a = a - 1;
   too.innerText = a;
+  const box = document.createElement("div");
+  box.setAttribute("class", "card");
+  container.appendChild(box);
 });
